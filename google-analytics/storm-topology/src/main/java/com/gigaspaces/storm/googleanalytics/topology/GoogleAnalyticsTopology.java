beb7e0e1ad492b32c40a153aa8d9923ebb359e8d
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2014-08-05 16:09:00 +0300

    fix for sliding window counter

--- google-analytics/storm-topology/src/main/java/com/gigaspaces/storm/googleanalytics/topology/GoogleAnalyticsTopology.java
+++ google-analytics/storm-topology/src/main/java/com/gigaspaces/storm/googleanalytics/topology/GoogleAnalyticsTopology.java
@@ -37,7 +37,7 @@ public class GoogleAnalyticsTopology {
 
             conf.put(ConfigConstants.XAP_SPACE_URL_KEY, "jini://" + gsmLocator + "/*/space");
             StormSubmitter.submitTopologyWithProgressBar(topologyName, conf, stormTopology);
-        } else if(args.length == 0){
+        } else if (args.length == 0) {
             conf.put(ConfigConstants.XAP_SPACE_URL_KEY, "jini://*/*/space");
             StormRunner.runTopologyLocally(stormTopology, "topology", conf, 100000);
         } else {
